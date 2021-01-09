f817dd482fea24df8bd963852f34c8830e530ba1
Shraddhey <siddhu1101@gmail.com>
2021-01-09 14:39:55 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "css")));
+
+app.get('/', (req, res, next) => {
+    res.render("payment-page", {
+        pageTitle: "Payment",
+    });
+})
+
+app.post('/', (req, res, next) => {
+    console.log(req);
+    console.log(req.body.cardname,req.body.cardnumber,req.body.expdate,req.body.cvv);
+})
+
+app.listen(4500);
